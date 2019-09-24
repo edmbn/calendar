2867f04467d953b60b8ee34518abf1049b4b2e71
Eduard Montfort Bonjoch <C0H@PF138T2D.costaisa.org>
2019-09-24 16:20:22 +0200

    Added correctly styles for selected dates

--- src/components/calendar/calendar.tsx
+++ src/components/calendar/calendar.tsx
@@ -1,4 +1,4 @@
-import { Component, Prop, h } from '@stencil/core';
+import { Component, Prop, h, Element, State } from '@stencil/core';
 
 @Component({
   tag: 'edmbn-calendar',
@@ -26,16 +26,37 @@ export class Calendar {
    */
   @Prop() type = 'range';
 
-  startDate: any;
-  endDate: any;
-  selectedElements: any[];
+  /**
+   * Component element in DOM
+   */
+  @Element() element: HTMLElement;
+
+  monthHeader: string;
+
+  @State() startDate: any;
+  @State() endDate: any;
+  monthObject: any[];
+  monthNames = ['ENE', 'FEB', 'MAR', 'ABR', 'MAY', 'JUN', 'JUL', 'AGO', 'SEP', 'OCT', 'NOV', 'DIC'];
+
+  componentWillLoad() {
+    this.getMonthView();
+  }
+  componentWillUpdate() {
+    this.getMonthView();
+  }
+
+  componentDidUpdate() {}
 
   private getMonthView() {
     const date = new Date();
-    if (this.year) {
+    date.setHours(0);
+    date.setMinutes(0);
+    date.setSeconds(0);
+    date.setMilliseconds(0);
+    if (this.year != undefined) {
       date.setFullYear(this.year);
     }
-    if (this.month) {
+    if (this.month != undefined) {
       date.setMonth(this.month);
     }
     return this.getMonthWeeks(date);
@@ -44,10 +65,13 @@ export class Calendar {
   private getMonthWeeks(date: Date) {
     const monthObject = [];
     const month = date.getMonth();
+    this.month = month;
+    this.year = date.getFullYear();
     let week = 0;
     let lastWeekDay;
     const currentMonthDay = date.getDate();
     date.setDate(1);
+    this.monthHeader = `${this.monthNames[date.getMonth()]} ${date.getFullYear()}`;
     for (let monthDay = 1; month === date.getMonth(); monthDay++) {
       const currentweekDay = this.getWeekDayByLocation(date.getDay());
       if (lastWeekDay && lastWeekDay > currentweekDay) {
@@ -67,7 +91,7 @@ export class Calendar {
       lastWeekDay = currentweekDay;
       date.setDate(monthDay + 1);
     }
-    return monthObject;
+    this.monthObject = monthObject;
   }
 
   getWeekDayByLocation(weekDay: number) {
@@ -83,67 +107,78 @@ export class Calendar {
     }
   }
 
-  resetCalendarSelection() {
-    if (this.selectedElements) {
-      this.selectedElements.forEach(el => {
-        el.classList.remove('selected-date', 'start-item', 'end-item');
-        if (el.firstChild.classList.contains('start-date') || el.firstChild.classList.contains('end-date')) {
-          el.firstChild.classList.remove('start-date', 'end-date');
-        }
-      });
-      this.selectedElements = [];
+  handleDayClick(timestamp: number) {
+    if (!timestamp) {
+      return;
     }
-    if (this.startDate) {
-      this.startDate.element.classList.remove('start-date');
+    if ((this.startDate && this.endDate) || (this.startDate && timestamp < this.startDate)) {
       this.startDate = null;
-    }
-    if (this.endDate) {
-      this.endDate.element.classList.remove('end-date');
       this.endDate = null;
     }
-  }
-
-  selectRangeDays() {
-    const calendarElements = Array.from(document.querySelector('edmbn-calendar').shadowRoot.querySelectorAll('.day'));
-    const startDayElement = calendarElements.find((el: any) => el.firstChild.classList.contains('start-date'));
-    const startDayIndex = calendarElements.indexOf(startDayElement);
-    const endDayElement = calendarElements.find((el: any) => el.firstChild.classList.contains('end-date'));
-    const endDayIndex = calendarElements.indexOf(endDayElement);
-
-    startDayElement.classList.add('start-item');
-    endDayElement.classList.add('end-item');
-    this.selectedElements = calendarElements.slice(startDayIndex, endDayIndex + 1);
-    this.selectedElements.forEach(el => el.classList.add('selected-date'));
-  }
-
-  handleDayClick(event, timestamp: number) {
-    if (this.startDate && this.endDate) {
-      this.resetCalendarSelection();
-    } else if (this.startDate && timestamp < this.startDate.timestamp) {
-      this.resetCalendarSelection();
-    }
-
     if (!this.startDate) {
-      const startElement = event.composedPath()[0];
-      startElement.classList.add('start-date');
-      this.startDate = { element: startElement, timestamp };
+      this.startDate = timestamp;
     } else if (!this.endDate) {
-      const endElement = event.composedPath()[0];
-      endElement.classList.add('end-date');
-      this.endDate = { element: endElement, timestamp };
-      this.selectRangeDays();
+      if (timestamp === this.startDate) {
+        this.startDate = this.startDate;
+      } else {
+        this.endDate = timestamp;
+      }
     }
+  }
+
+  changeMonth(direction: string) {
+    if (direction === 'next') {
+      this.month += 1;
+    } else {
+      this.month -= 1;
+    }
+  }
+
+  getSelectionStyles(timestamp) {
+    const elementTime = parseInt(timestamp);
+    let selectionStyles = '';
+    if (!this.startDate && !this.endDate) {
+      return selectionStyles;
+    }
+    if (this.startDate) {
+      if (elementTime === this.startDate) {
+        selectionStyles = 'start-date';
+      }
+    }
+    if (this.endDate) {
+      if (elementTime === this.endDate) {
+        selectionStyles = 'end-date';
+      }
+    }
+    if (this.startDate && this.endDate && (elementTime >= this.startDate && elementTime <= this.endDate)) {
+      selectionStyles = `${selectionStyles} selected-date`;
+    }
+    return selectionStyles;
   }
 
   render() {
     return (
       <div class='flex flex-col items-center'>
         <header class='flex justify-between items-center w-4/5'>
-          <svg class='w-1/6 max-w-2' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 512 512'>
+          <svg
+            class='w-1/6 max-w-2'
+            xmlns='http://www.w3.org/2000/svg'
+            viewBox='0 0 512 512'
+            onClick={() => {
+              this.changeMonth('previous');
+            }}
+          >
             <path d='M217.9 256L345 129c9.4-9.4 9.4-24.6 0-33.9-9.4-9.4-24.6-9.3-34 0L167 239c-9.1 9.1-9.3 23.7-.7 33.1L310.9 417c4.7 4.7 10.9 7 17 7s12.3-2.3 17-7c9.4-9.4 9.4-24.6 0-33.9L217.9 256z' />
           </svg>
-          <span>MONTH</span>
-          <svg class='w-1/6 max-w-2' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 512 512'>
+          <span>{this.monthHeader}</span>
+          <svg
+            class='w-1/6 max-w-2'
+            xmlns='http://www.w3.org/2000/svg'
+            viewBox='0 0 512 512'
+            onClick={() => {
+              this.changeMonth('next');
+            }}
+          >
             <path d='M294.1 256L167 129c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.3 34 0L345 239c9.1 9.1 9.3 23.7.7 33.1L201.1 417c-4.7 4.7-10.9 7-17 7s-12.3-2.3-17-7c-9.4-9.4-9.4-24.6 0-33.9l127-127.1z' />
           </svg>
         </header>
@@ -158,18 +193,24 @@ export class Calendar {
           <li class='w-1/7 flex justify-center'>Dom</li>
         </ul>
         <div class='flex flex-col w-full'>
-          {this.getMonthView().map(week => (
+          {this.monthObject.map(week => (
             <ul class='flex'>
               {week.map(day => (
-                <li class={`w-1/7 flex justify-center items-center day`}>
-                  <span
-                    onClick={event => {
-                      this.handleDayClick(event, day.timestamp);
+                <li
+                  data-time={day ? day.timestamp : false}
+                  class={`w-1/7 flex justify-center items-center day outline-none ${
+                    day ? this.getSelectionStyles(day.timestamp) : ''
+                  }`}
+                >
+                  <button
+                    disabled={!day}
+                    class='w-full h-full flex items-center justify-center'
+                    onClick={() => {
+                      this.handleDayClick(day.timestamp);
                     }}
-                    class={`day-text rounded-full`}
                   >
-                    {day ? day.number : ''}
-                  </span>
+                    <span class={`day-text rounded-full`}>{day ? day.number : ''}</span>
+                  </button>
                 </li>
               ))}
             </ul>
